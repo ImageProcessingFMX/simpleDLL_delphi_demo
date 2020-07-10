722847cd0ce58d774628b7a6765a6011a4b3a587
ImageProcessingFMX <41538979+ImageProcessingFMX@users.noreply.github.com>
2020-07-10 14:13:35 +0200

    Add files via upload
    inital commmit / copy files

---
+++ Project_dlltest.dpr
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+program Project_dlltest;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit_simpleDLLTEST in 'Unit_simpleDLLTEST.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+=======
+program Project_dlltest;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit_simpleDLLTEST in 'Unit_simpleDLLTEST.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+>>>>>>> bbb2158d43dfecb05c66c2f26ff5e1cc9180373d
