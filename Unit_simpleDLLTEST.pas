db78320493f12eee5d502f5de5acb9dbae5bcca9
ImageProcessingFMX <41538979+ImageProcessingFMX@users.noreply.github.com>
2020-07-11 15:54:51 +0200

    Add files via upload

--- Unit_simpleDLLTEST.pas
+++ Unit_simpleDLLTEST.pas
@@ -3,7 +3,8 @@ unit Unit_simpleDLLTEST;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.ScrollBox,
   FMX.Memo, FMX.StdCtrls, FMX.Edit, FMX.EditBox, FMX.SpinBox,
   FMX.Controls.Presentation;
@@ -16,7 +17,9 @@ type
     lbl1: TLabel;
     lbl2: TLabel;
     mmo_out: TMemo;
+    btn_minus2numbers: TButton;
     procedure btn_add2numbersClick(Sender: TObject);
+    procedure btn_minus2numbersClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -26,28 +29,43 @@ type
 var
   Form1: TForm1;
 
-
 {$IFDEF  MSWINDOWS}
-function AddIntegers(_a, _b: integer): integer; stdcall; external 'TestLibrary.dll';
+function AddIntegers(_a, _b: integer): integer; stdcall;
+  external 'TestLibrary.dll';
+
+function MinusIntegers(_a, _b: integer): integer; stdcall;
+  external 'TestLibrary.dll';
 {$ENDIF}
-
-
 {$IFDEF  LINUX}
-  function AddIntegers(_a, _b: integer): integer; stdcall; external 'libTestLibrary.so';
-{$ENDIF}
+function AddIntegers(_a, _b: integer): integer; stdcall;
+  external 'libTestLibrary.so';
 
+function MinusIntegers(_a, _b: integer): integer; stdcall;
+  external 'libTestLibrary.so';
+{$ENDIF}
 
 implementation
 
 {$R *.fmx}
 
 procedure TForm1.btn_add2numbersClick(Sender: TObject);
-var   a,b,c : Integer ;
+var
+  a, b, c: integer;
 begin
-    a :=  round(spnbx_A.Value);
-    b :=  Round(spnbx_B.Value);
-    c := AddIntegers (a,b);
-    mmo_out.Lines.Add('Add : '  +  IntToStr(c));
+  a := round(spnbx_A.Value);
+  b := round(spnbx_B.Value);
+  c := AddIntegers(a, b);
+  mmo_out.Lines.Add('Add    C:= ' + IntToStr(c));
+end;
+
+procedure TForm1.btn_minus2numbersClick(Sender: TObject);
+var
+  a, b, c: integer;
+begin
+  a := round(spnbx_A.Value);
+  b := round(spnbx_B.Value);
+  c := MinusIntegers(a, b);
+  mmo_out.Lines.Add('Minus   C:= ' + IntToStr(c));
 end;
 
 end.
