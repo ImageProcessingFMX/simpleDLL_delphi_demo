7b41a8370f249beee9b70e8ac1ad924ae32c73a6
ImageProcessingFMX <41538979+ImageProcessingFMX@users.noreply.github.com>
2020-07-11 15:45:48 +0200

    git faillure fixed

--- Project_dlltest.dpr
+++ Project_dlltest.dpr
@@ -12,19 +12,3 @@ begin
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
-=======
-program Project_dlltest;
-
-uses
-  System.StartUpCopy,
-  FMX.Forms,
-  Unit_simpleDLLTEST in 'Unit_simpleDLLTEST.pas' {Form1};
-
-{$R *.res}
-
-begin
-  Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
-  Application.Run;
-end.
-
